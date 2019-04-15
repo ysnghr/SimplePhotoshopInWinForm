a193575f1398b693965083728dd3c4f2141b6921
ysnghr <yasinyg@code.edu.az>
2019-04-15 20:32:20 +0400

    SimplePhotoshopInWinForm

---
+++ ImageEffect/ImageEffect/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace ImageEffect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbx1 = new System.Windows.Forms.PictureBox();
+            this.pbx2 = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.effect1 = new System.Windows.Forms.Button();
+            this.effect2 = new System.Windows.Forms.Button();
+            this.effect3 = new System.Windows.Forms.Button();
+            this.effect4 = new System.Windows.Forms.Button();
+            this.effect5 = new System.Windows.Forms.Button();
+            this.effect6 = new System.Windows.Forms.Button();
+            this.btnupload = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.effect7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbx1
+            // 
+            this.pbx1.ImageLocation = "C:\\Users\\Acer\\Desktop\\image-1465348_960_720.jpg";
+            this.pbx1.Location = new System.Drawing.Point(17, 85);
+            this.pbx1.Name = "pbx1";
+            this.pbx1.Size = new System.Drawing.Size(397, 367);
+            this.pbx1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx1.TabIndex = 0;
+            this.pbx1.TabStop = false;
+            this.pbx1.Paint += new System.Windows.Forms.PaintEventHandler(this.pbx1_Paint);
+            this.pbx1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbx1_MouseDown);
+            this.pbx1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbx1_MouseMove);
+            this.pbx1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbx1_MouseUp);
+            // 
+            // pbx2
+            // 
+            this.pbx2.Location = new System.Drawing.Point(444, 85);
+            this.pbx2.Name = "pbx2";
+            this.pbx2.Size = new System.Drawing.Size(397, 367);
+            this.pbx2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbx2.TabIndex = 1;
+            this.pbx2.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.effect1);
+            this.flowLayoutPanel1.Controls.Add(this.effect2);
+            this.flowLayoutPanel1.Controls.Add(this.effect3);
+            this.flowLayoutPanel1.Controls.Add(this.effect4);
+            this.flowLayoutPanel1.Controls.Add(this.effect5);
+            this.flowLayoutPanel1.Controls.Add(this.effect6);
+            this.flowLayoutPanel1.Controls.Add(this.effect7);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 458);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(853, 91);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // effect1
+            // 
+            this.effect1.Location = new System.Drawing.Point(3, 7);
+            this.effect1.Name = "effect1";
+            this.effect1.Size = new System.Drawing.Size(135, 64);
+            this.effect1.TabIndex = 0;
+            this.effect1.Text = "Effect 1";
+            this.effect1.UseVisualStyleBackColor = true;
+            this.effect1.Click += new System.EventHandler(this.effect1_Click);
+            // 
+            // effect2
+            // 
+            this.effect2.Location = new System.Drawing.Point(144, 7);
+            this.effect2.Name = "effect2";
+            this.effect2.Size = new System.Drawing.Size(135, 64);
+            this.effect2.TabIndex = 1;
+            this.effect2.Text = "Effect 2";
+            this.effect2.UseVisualStyleBackColor = true;
+            this.effect2.Click += new System.EventHandler(this.effect2_Click);
+            // 
+            // effect3
+            // 
+            this.effect3.Location = new System.Drawing.Point(285, 7);
+            this.effect3.Name = "effect3";
+            this.effect3.Size = new System.Drawing.Size(135, 64);
+            this.effect3.TabIndex = 2;
+            this.effect3.Text = "Effect 3";
+            this.effect3.UseVisualStyleBackColor = true;
+            this.effect3.Click += new System.EventHandler(this.effect3_Click);
+            // 
+            // effect4
+            // 
+            this.effect4.Location = new System.Drawing.Point(426, 7);
+            this.effect4.Name = "effect4";
+            this.effect4.Size = new System.Drawing.Size(135, 64);
+            this.effect4.TabIndex = 3;
+            this.effect4.Text = "Effect 4";
+            this.effect4.UseVisualStyleBackColor = true;
+            this.effect4.Click += new System.EventHandler(this.effect4_Click);
+            // 
+            // effect5
+            // 
+            this.effect5.Location = new System.Drawing.Point(567, 7);
+            this.effect5.Name = "effect5";
+            this.effect5.Size = new System.Drawing.Size(135, 64);
+            this.effect5.TabIndex = 4;
+            this.effect5.Text = "Effect 5";
+            this.effect5.UseVisualStyleBackColor = true;
+            this.effect5.Click += new System.EventHandler(this.effect5_Click);
+            // 
+            // effect6
+            // 
+            this.effect6.Location = new System.Drawing.Point(708, 7);
+            this.effect6.Name = "effect6";
+            this.effect6.Size = new System.Drawing.Size(135, 64);
+            this.effect6.TabIndex = 5;
+            this.effect6.Text = "Effect 6";
+            this.effect6.UseVisualStyleBackColor = true;
+            this.effect6.Click += new System.EventHandler(this.effect6_Click);
+            // 
+            // btnupload
+            // 
+            this.btnupload.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupload.Location = new System.Drawing.Point(17, 43);
+            this.btnupload.Name = "btnupload";
+            this.btnupload.Size = new System.Drawing.Size(397, 36);
+            this.btnupload.TabIndex = 3;
+            this.btnupload.Text = "Upload Image";
+            this.btnupload.UseVisualStyleBackColor = true;
+            this.btnupload.Click += new System.EventHandler(this.btnupload_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.Location = new System.Drawing.Point(444, 43);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(397, 36);
+            this.btnsave.TabIndex = 4;
+            this.btnsave.Text = "Save Image";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // effect7
+            // 
+            this.effect7.Location = new System.Drawing.Point(849, 7);
+            this.effect7.Name = "effect7";
+            this.effect7.Size = new System.Drawing.Size(135, 64);
+            this.effect7.TabIndex = 6;
+            this.effect7.Text = "Effect 7";
+            this.effect7.UseVisualStyleBackColor = true;
+            this.effect7.Click += new System.EventHandler(this.effect7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 549);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btnupload);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.pbx2);
+            this.Controls.Add(this.pbx1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbx1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx2)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbx1;
+        private System.Windows.Forms.PictureBox pbx2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnupload;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.Button effect1;
+        private System.Windows.Forms.Button effect2;
+        private System.Windows.Forms.Button effect3;
+        private System.Windows.Forms.Button effect4;
+        private System.Windows.Forms.Button effect5;
+        private System.Windows.Forms.Button effect6;
+        private System.Windows.Forms.Button effect7;
+    }
+}
+
---
+++ ImageEffect/ImageEffect/Form1.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageEffect
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect;
+        Bitmap cropedphoto;
+        Point LocationXY;
+        Point LocationX1Y1;
+        bool MouseDownIs = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pbx1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MouseDownIs = true;
+            LocationXY = e.Location;
+        }
+
+        private void pbx1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseDownIs == true)
+            {
+                LocationX1Y1 = e.Location;
+                Refresh();
+            }
+        }
+
+        private void pbx1_MouseUp(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (MouseDownIs == true)
+                {
+                    LocationX1Y1 = e.Location;
+                    MouseDownIs = false;
+                    if (rect !=null)
+                    {
+                        Bitmap bit = new Bitmap(pbx1.Image, pbx1.Width, pbx1.Height);
+                        Bitmap crop = new Bitmap(rect.Width, rect.Height);
+
+                        Graphics g = Graphics.FromImage(crop);
+                        g.DrawImage(bit, 0, 0, rect, GraphicsUnit.Pixel);
+                        pbx2.Image = crop;
+                        cropedphoto = crop;
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void pbx1_Paint(object sender, PaintEventArgs e)
+        {
+            if (rect!=null)
+            {
+                e.Graphics.DrawRectangle(Pens.Red, GetRect());
+
+            }
+        }
+
+        private Rectangle GetRect()
+        {
+            rect= new Rectangle();
+            rect.X = Math.Min(LocationXY.X, LocationX1Y1.X);
+            rect.Y = Math.Min(LocationXY.Y, LocationX1Y1.Y);
+
+            rect.Width = Math.Abs(LocationXY.X - LocationX1Y1.X);
+            rect.Height = Math.Abs(LocationXY.Y - LocationX1Y1.Y);
+            return rect;
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            if (pbx2.Image !=null)
+            {
+                SaveFileDialog saveimg = new SaveFileDialog();
+                saveimg.Filter = "Image Files(*.gif,*.jpg,*.jpeg,*.png)|*.gif,*.jpg,*.jpeg,*.png";
+                if (saveimg.ShowDialog()== DialogResult.OK)
+                {
+                    if (saveimg.FileName.EndsWith(".jpg"))
+                    {
+                        pbx2.Image.Save(saveimg.FileName, ImageFormat.Bmp);
+                    }
+                    else if (saveimg.FileName.EndsWith(".gif"))
+                    {
+                        pbx2.Image.Save(saveimg.FileName, ImageFormat.Gif);
+                    }
+                    else if (saveimg.FileName.EndsWith(".jpeg"))
+                    {
+                        pbx2.Image.Save(saveimg.FileName, ImageFormat.Jpeg);
+                    }
+                    else if (saveimg.FileName.EndsWith(".png"))
+                    {
+                        pbx2.Image.Save(saveimg.FileName, ImageFormat.Png);
+                    }
+                }
+            }
+        }
+
+        private void btnupload_Click(object sender, EventArgs e)
+        {
+
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    pbx1.Image = new Bitmap(dlg.FileName);
+                }
+            }
+        }
+
+        private void effect1_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width;x++)
+                {
+                    Color p = bmp.GetPixel(x,y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x,y, Color.FromArgb(a, r, 40, 20));
+                    gbmp.SetPixel(x,y, Color.FromArgb(a, 0, g, 0));
+                    bbmp.SetPixel(x,y, Color.FromArgb(a, 0, 0, b));
+
+                }
+            }
+            pbx2.Image = rbmp;
+        }
+
+        private void effect2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x, y, Color.FromArgb(a, r, 40, 20));
+                    gbmp.SetPixel(x, y, Color.FromArgb(a, 0, g, 0));
+                    bbmp.SetPixel(x, y, Color.FromArgb(a, 0, 0, b));
+
+                }
+            }
+            pbx2.Image = gbmp;
+        }
+
+        private void effect3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x, y, Color.FromArgb(a, r, 40, 20));
+                    gbmp.SetPixel(x, y, Color.FromArgb(a, 0, g, 0));
+                    bbmp.SetPixel(x, y, Color.FromArgb(a, 0, 0, b));
+
+                }
+            }
+            pbx2.Image = bbmp;
+        }
+
+        private void effect4_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x, y, Color.FromArgb(a, r, 40, 20));
+                    gbmp.SetPixel(x, y, Color.FromArgb(a, 0, g, 0));
+                    bbmp.SetPixel(x, y, Color.FromArgb(b, 0, 50, r));
+
+                }
+            }
+            pbx2.Image = bbmp;
+        }
+
+        private void effect5_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x, y, Color.FromArgb(a, r, 40, 20));
+                    gbmp.SetPixel(x, y, Color.FromArgb(g, 0, g, 0));
+                    bbmp.SetPixel(x, y, Color.FromArgb(b, 0, 50, r));
+
+                }
+            }
+            pbx2.Image = gbmp;
+        }
+
+        private void effect6_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x, y, Color.FromArgb(g, b, 40, 20));
+                    gbmp.SetPixel(x, y, Color.FromArgb(g, 0, g, 0));
+                    bbmp.SetPixel(x, y, Color.FromArgb(b, 0, 50, r));
+
+                }
+            }
+            pbx2.Image = rbmp;
+        }
+
+        private void effect7_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = cropedphoto;
+            int width = bmp.Width;
+            int height = bmp.Height;
+            Bitmap rbmp = new Bitmap(bmp);
+            Bitmap gbmp = new Bitmap(bmp);
+            Bitmap bbmp = new Bitmap(bmp);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    int a = p.A;
+                    int r = p.R;
+                    int g = p.G;
+                    int b = p.B;
+                    rbmp.SetPixel(x, y, Color.FromArgb(r, b, g, 20));
+                    gbmp.SetPixel(x, y, Color.FromArgb(g, 0, g, 0));
+                    bbmp.SetPixel(x, y, Color.FromArgb(b, 0, 50, r));
+
+                }
+            }
+            pbx2.Image = rbmp;
+        }
+    }
+}
